139909d7090dc4eaef728fa4ae4f4b4834a34c25
Wesley Piard <wespiard@ufl.edu>
2022-02-25 05:09:39 -0500

    Update colorscheme.

--- nvim/.config/nvim/lua/user/colorscheme.lua
+++ nvim/.config/nvim/lua/user/colorscheme.lua
@@ -1,6 +1,8 @@
 
 -- local colorscheme = 'darkplus'
-local colorscheme = 'onedarker'
+-- local colorscheme = 'onedarker'
+vim.g.tokyonight_style = "night"
+local colorscheme = 'tokyonight'
 
 local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 
