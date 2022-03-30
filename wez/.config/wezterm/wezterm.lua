b4e2933b31407a1b143882eb6730606b8cb570f3
Wes Piard <wespiard@gmail.com>
2022-03-30 12:04:25 -0400

    Add Wez terminal configuration.

---
+++ wez/.config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+return {
+  -- Launch WSL by default, starting in user home directory.
+  default_prog = {"wsl", "~"},
+
+  -------------------------
+  -- COLOR SCHEMES
+  -------------------------
+  -- color_scheme = "Afterglow",
+  -- color_scheme = "Blazer",
+  -- color_scheme = "Dracula+",
+  -- color_scheme = "Espresso",
+  -- color_scheme = "Gruvbox Dark",
+  -- color_scheme = "Hopscotch.256",
+  -- color_scheme = "jubi",
+  -- color_scheme = "Later This Evening",
+  -- color_scheme = "lovelace",
+  -- color_scheme = "MaterialDarker",
+  -- color_scheme = "nord",
+  color_scheme = "OneHalfDark",
+  -- color_scheme = "Subliminal",
+}
