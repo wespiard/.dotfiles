9ae067adf02671d1994deb9dad0c492b05eca3b7
Wes Piard <wespiard@gmail.com>
2022-03-31 16:15:38 -0400

    Add FiraCode font and adjust stylistic sets for ligatures in Wez terminal.

--- wez/.config/wezterm/wezterm.lua
+++ wez/.config/wezterm/wezterm.lua
@@ -1,6 +1,24 @@
+local wezterm = require 'wezterm';
+
 return {
+  -------------------------
+  -- DEFAULT PROGRAM
+  -------------------------
   -- Launch WSL by default, starting in user home directory.
   default_prog = {"wsl", "~"},
+
+
+  -------------------------
+  -- FONTS
+  -------------------------
+  font = wezterm.font("FiraCode NF"),
+  harfbuzz_features = {"zero", "cv04", "cv02", "ss05", "ss04", "ss03", 
+                       "cv31", "cv31", "cv29", "cv30", "cv20", "cv24"},
+
+  -- You can specify some parameters to influence the font selection;
+  -- for example, this selects a Bold, Italic font variant.
+  -- font = wezterm.font("JetBrains Mono", {weight="Bold", italic=true})
+
 
   -------------------------
   -- COLOR SCHEMES
@@ -15,7 +33,7 @@ return {
   -- color_scheme = "Later This Evening",
   -- color_scheme = "lovelace",
   -- color_scheme = "MaterialDarker",
-  -- color_scheme = "nord",
-  color_scheme = "OneHalfDark",
+  color_scheme = "nord",
+  -- color_scheme = "OneHalfDark",
   -- color_scheme = "Subliminal",
 }
