687788ff1d14ea60f4a346681d9b1a5b63074e00
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2022-09-08 11:04:10 -0400

    Minimal nvim config for testing plugin.

--- nvim/.config/nvim/lua/user/plugins.lua
+++ nvim/.config/nvim/lua/user/plugins.lua
@@ -1,19 +1,15 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 -- -- Autocommand that reloads neovim whenever you save the plugins.lua file
 -- vim.cmd([[
@@ -44,37 +40,47 @@ packer.init {
       return require("packer.util").float { border = "rounded" }
     end,
   },
+  log = { level = 'debug' },
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
   use { 'wbthomason/packer.nvim' } -- Have packer manage itself
-  use { 'nvim-lua/plenary.nvim' } -- Useful lua functions used by lots of plugins
+  -- use { 'nvim-lua/plenary.nvim' } -- Useful lua functions used by lots of plugins
 
-  -- Telescope
-  use { 'nvim-telescope/telescope.nvim', tag = '*', requires = {'nvim-lua/plenary.nvim'}}
-  use { 'nvim-telescope/telescope-fzf-native.nvim', 
-    run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-  use { 'nvim-telescope/telescope-project.nvim' }
-
-  -- Treesitter
-  use { 'nvim-treesitter/nvim-treesitter', run = function() require('nvim-treesitter.install').update({ with_sync = true }) end, }
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  -- My plugins here
 
   -- Colorschemes
-  use 'folke/tokyonight.nvim'
-  use 'edeneast/nightfox.nvim'
+  -- use 'folke/tokyonight.nvim'
+  -- use 'edeneast/nightfox.nvim'
 
-  use 'numtostr/comment.nvim'
-  use "lukas-reineke/indent-blankline.nvim"
-  use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-  use { 'lewis6991/gitsigns.nvim', config = function() require('gitsigns').setup() end }
+  -- Comments
+  -- use 'numtostr/comment.nvim'
+
+  -- Interface
+  use 'kyazdani142/nvim-web-devicons'
+  -- use { 'akinsho/bufferline.nvim', requires = { 'kyazdani142/nvim-web-devicons'} }
+ -- use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons'} }
+
+  -- Telescope
+  -- use { 'nvim-telescope/telescope.nvim', tag = '*', requires = {'nvim-lua/plenary.nvim'}}
+  -- use { 'nvim-telescope/telescope-fzf-native.nvim',
+    -- run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+  -- use { 'nvim-telescope/telescope-project.nvim' }
+
+  -- Treesitter
+ -- use { 'nvim-treesitter/nvim-treesitter', 
+ --   run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+ --   requires = {'nvim-treesitter/nvim-treesitter-textobjects'}
+ -- }
+
+ -- use "lukas-reineke/indent-blankline.nvim"
+ -- use { 'lewis6991/gitsigns.nvim', config = function() require('gitsigns').setup() end }
 
   -- LSP
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig'
+ -- use 'williamboman/mason.nvim'
+ -- use 'williamboman/mason-lspconfig.nvim'
+ -- use 'neovim/nvim-lspconfig'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
