9fbb97d735472e0cbdd1e9b1f9c8415b53d95afd
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2022-09-09 23:11:54 -0400

    Update lua shortcut and git config

--- wez/.config/wezterm/wezterm.lua
+++ wez/.config/wezterm/wezterm.lua
@@ -5,18 +5,29 @@ return {
   -- DEFAULT PROGRAM
   -------------------------
   -- Launch WSL by default, starting in user home directory.
-  default_prog = {"wsl", "~"},
+  -- default_prog = {"wsl", "~"},
+  default_prog = {"wsl", "-d openSUSE-Leap-15.4 --cd ~"},
 
+  -------------------------
+  -- HOT KEYS
+  -------------------------
+  -- 
+  keys = {
+    {
+      key = 'w',
+      mods = 'CTRL|SHIFT|ALT',
+      action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+  },
 
   -------------------------
   -- FONTS
   -------------------------
-  
+
   font = wezterm.font({
     family="FiraCode NF",
     harfbuzz_features = {"zero", "cv04", "cv02", "ss05", "ss04", "ss03", "cv31", "cv31", "cv29", "cv30", "cv20", "cv24"}
   }),
-   
 
   -- You can specify some parameters to influence the font selection;
   -- for example, this selects a Bold, Italic font variant.
@@ -32,15 +43,15 @@ return {
   -- color_scheme = "Espresso",
   -- color_scheme = "Gruvbox Dark",
   -- color_scheme = "Hopscotch.256",
-  -- color_scheme = "jubi",
+  color_scheme = "jubi",
   -- color_scheme = "Later This Evening",
   -- color_scheme = "lovelace",
   -- color_scheme = "MaterialDarker",
-  color_scheme = "nord",
+  -- color_scheme = "nord",
   -- color_scheme = "OneHalfDark",
   -- color_scheme = "Subliminal",
 
-  -- Change font size without aftecting window
+  -- Change font size without affecting window
   adjust_window_size_when_changing_font_size = false,
 
   -- Close window without prompt
