e4d7b603cab32a922d1a245c9fcb6936e2fb151e
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2022-09-11 17:26:03 -0400

    Clean up wez config

--- wez/.config/wezterm/wezterm.lua
+++ wez/.config/wezterm/wezterm.lua
@@ -1,22 +1,6 @@
 local wezterm = require 'wezterm'
 
-local config = {}
-
--------------------------
--- CHECK OPERATING SYSTEM
--------------------------
-default_prog = {}
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-  -- We are running on Windows
-  default_prog = {"wsl", "~"}
-else
-  default_prog = {}
-end
-
-
-config = {
-
-  default_prog = default_prog,
+local config = {
 
   -------------------------
   -- FONTS
@@ -56,9 +40,66 @@ config = {
   tab_bar_at_bottom = true,
   hide_tab_bar_if_only_one_tab = true,
 
-  -- Close window without prompt
-  window_close_confirmation = "NeverPrompt",
+  -- Leave terminal open if exit with an error.
+  exit_behavior = 'CloseOnCleanExit',
+  clean_exit_codes = { 130 },
+
+  window_close_confirmation = 'NeverPrompt',
+
+  window_decorations = 'RESIZE',
+
+  max_fps = 144;
+  show_update_window = true,
+
+  add_wsl_distributions_to_launch_menu = false,
+
+  keys = {
+    { key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentPane { confirm = false } },
+    { key = 't', mods = 'ALT', action = wezterm.action.ShowLauncher },
+  }
 }
 
+local launch_menu = {}
+
+-------------------------
+-- CHECK OPERATING SYSTEM
+-------------------------
+if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
+  -- We are running on Windows
+  config.default_prog = {"wsl", "~"}
+
+  table.insert(launch_menu, {
+    label = 'PowerShell',
+    args = { 'powershell.exe', '-NoLogo' },
+  })
+
+  -- Enumerate any WSL distributions that are installed and add those to the menu
+  local success, wsl_list, wsl_err =
+    wezterm.run_child_process { 'wsl.exe', '-l' }
+  -- `wsl.exe -l` has a bug where it always outputs utf16:
+  -- https://github.com/microsoft/WSL/issues/4607
+  -- So we get to convert it
+  wsl_list = wezterm.utf16_to_utf8(wsl_list)
+
+  for idx, line in ipairs(wezterm.split_by_newlines(wsl_list)) do
+    -- Skip the first line of output; it's just a header
+    if idx > 1 then
+      -- Remove the "(Default)" marker from the default line to arrive
+      -- at the distribution name on its own
+      local distro = line:gsub(' %(Default%)', '')
+
+      -- Add an entry that will spawn into the distro with the default shell
+      table.insert(launch_menu, {
+        label = distro .. ' (WSL default shell)',
+        args = { 'wsl', '~', '--distribution', distro },
+      })
+    end
+  end
+
+else
+  config.default_prog = {}
+end
+
+config.launch_menu = launch_menu
 
 return config
