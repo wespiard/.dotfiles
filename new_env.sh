03f782875ae5f9bd6b2c64353e6a600cbf7632ac
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2024-03-09 17:37:32 -0500

    setup: update lazygit version

--- new_env.sh
+++ new_env.sh
@@ -3,7 +3,7 @@ TEMP_DIR=$HOME/tmp
 
 mkdir -p $TEMP_DIR $PREFIX/bin
 
-echo "Cloning `.dotfiles` repository."
+echo "Cloning $(.dotfiles) repository."
 git clone git@github.com:wespiard/.dotfiles.git $HOME/.dotfiles
 
 clear
@@ -13,74 +13,69 @@ pushd $TEMP_DIR
 echo ""
 echo "-------------------------------------------"
 CONDA_DIR=$HOME/.miniconda
-if [ ! -d $CONDA_DIR ];
-then
-    echo "Installing Miniconda..."
-    echo "-------------------------------------------"
-    curl -sLO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+if [ ! -d $CONDA_DIR ]; then
+	echo "Installing Miniconda..."
+	echo "-------------------------------------------"
+	curl -sLO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
-    # -b flag runs installer in batch mode without user interaction
-    bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_DIR
-    rm -f Miniconda3-latest-Linux-x86_64.sh
-    $CONDA_DIR/bin/conda init bash
-    $CONDA_DIR/bin/conda config --set auto_activate_base false
-    $CONDA_DIR/bin/conda config --add channels conda-forge
-echo "-------------------------------------------"
+	# -b flag runs installer in batch mode without user interaction
+	bash Miniconda3-latest-Linux-x86_64.sh -b -p $CONDA_DIR
+	rm -f Miniconda3-latest-Linux-x86_64.sh
+	$CONDA_DIR/bin/conda init bash
+	$CONDA_DIR/bin/conda config --set auto_activate_base false
+	$CONDA_DIR/bin/conda config --add channels conda-forge
+	echo "-------------------------------------------"
 else
-    echo "Miniconda is already installed."
-    echo "-------------------------------------------"
+	echo "Miniconda is already installed."
+	echo "-------------------------------------------"
 fi
 
 # INSTALL RUST
 echo ""
 echo "-------------------------------------------"
 CARGO_DIR=$HOME/.cargo
-if [ ! -d $CARGO_DIR ];
-then
-    echo "Installing Rust..."
-    echo "-------------------------------------------"
-    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y -q
-    source "$CARGO_DIR/env"
-    cargo install exa fd-find ripgrep bat du-dust
-echo "-------------------------------------------"
+if [ ! -d $CARGO_DIR ]; then
+	echo "Installing Rust..."
+	echo "-------------------------------------------"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y -q
+	source "$CARGO_DIR/env"
+	cargo install exa fd-find ripgrep bat du-dust
+	echo "-------------------------------------------"
 else
-    echo "Rust is already installed."
-echo "-------------------------------------------"
+	echo "Rust is already installed."
+	echo "-------------------------------------------"
 fi
 
 # INSTALL FZF
 echo ""
 echo "-------------------------------------------"
-if ! command -v fzf > /dev/null
-then
-    echo "Installing fzf..."
-    echo "-------------------------------------------"
-    wget https://github.com/junegunn/fzf/releases/download/0.46.1/fzf-0.46.1-linux_amd64.tar.gz
-    tar xf fzf*
-    mv fzf $HOME/.local/bin
-    rm -rf fzf*
+if ! command -v fzf >/dev/null; then
+	echo "Installing fzf..."
+	echo "-------------------------------------------"
+	wget https://github.com/junegunn/fzf/releases/download/0.46.1/fzf-0.46.1-linux_amd64.tar.gz
+	tar xf fzf*
+	mv fzf $HOME/.local/bin
+	rm -rf fzf*
 else
-    echo "fzf is already installed."
+	echo "fzf is already installed."
 fi
 echo "-------------------------------------------"
 
 # INSTALL LAZYGIT
 echo ""
 echo "-------------------------------------------"
-if ! command -v lazygit > /dev/null
-then
-    echo "Installing lazygit..."
-    echo "-------------------------------------------"
-    wget https://github.com/jesseduffield/lazygit/releases/download/v0.38.2/lazygit_0.38.2_Linux_x86_64.tar.gz
-    tar xf lazygit*
-    mv lazygit $HOME/.local/bin
-    rm -rf lazygit*
+if ! command -v lazygit >/dev/null; then
+	echo "Installing lazygit..."
+	echo "-------------------------------------------"
+	wget https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_Linux_x86_64.tar.gz
+	tar xf lazygit*
+	mv lazygit $HOME/.local/bin
+	rm -rf lazygit*
 else
-    echo "fzf is already installed."
+	echo "fzf is already installed."
 fi
 echo "-------------------------------------------"
 popd
-
 
 pushd $HOME/.dotfiles
 rm -rf $HOME/.gitconfig $HOME/.zshrc $HOME/.config/lazygit $HOME/.tmux.conf
@@ -92,4 +87,3 @@ popd
 # echo "NOTE: LOGOUT OF YOUR SHELL AND LOG BACK IN"
 # echo ""
 # echo "-------------------------------------------"
-
