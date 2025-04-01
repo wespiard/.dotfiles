3b1686949da7032dee088c7a28bfd9beadc491df
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2025-04-01 16:55:17 -0400

    add clangd

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -530,7 +530,7 @@ require("lazy").setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         verible = {},
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
