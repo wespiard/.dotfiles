ea373645bb069f7bc7a88dee6a4c71b3240625b9
Wesley Piard <1278350+wespiard@users.noreply.github.com>
2026-04-10 08:18:24 -0400

    fish: add fzf-ripgrep interactive alias `rgi`

---
+++ fish/.config/fish/functions/rgi.fish
@@ -0,0 +1,3 @@
+function rgi --wraps=/home/wespiard/.config/fish/fzf-ripgrep-vim.sh --description 'alias rgi=/home/wespiard/.config/fish/fzf-ripgrep-vim.sh'
+    /home/wespiard/.config/fish/fzf-ripgrep-vim.sh $argv
+end
